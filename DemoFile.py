886b14f91cd23ca7d96e7ad010c821ff95217f3e
Yogesh <ymugoder@netapp.com>
2025-03-03 11:37:09 +0530

    Add new file created by the script

--- DemoFile.py
+++ DemoFile.py
@@ -8,4 +8,5 @@ result = add_numbers(3, 5)
 print("The sum is:", result)
 # Create a new file and write some content to it
 with open("new_file.txt", "w") as file:
+    file.write("This is a new file created by the script." )  
     file.write("This is a new file created by the script.")
